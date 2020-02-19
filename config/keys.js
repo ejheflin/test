0fdf50a983f35234d74366a4f94a06183f4b333b
eric <eric@scorpio.localdomain>
2020-02-19 16:49:06 -0600

    added keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '867515371063-jr65o5gdk2lcj8hnqtq3c55288t8p8tu.apps.googleusercontent.com',
+	googleClientSecret: 'hL3jhS-4IelghSqu8ha90ZzN',
+	mongoURI: 'mongodb+srv://ejheflin:MBawuEM987aI@cluster0-ucznz.mongodb.net/test?retryWrites=true&w=majority',
+	cookieKey: 'kkjsdchsuckanllcnbvksjdv'
+};
